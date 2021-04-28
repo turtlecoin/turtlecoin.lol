b874a55c6032ac4c120c3e6c17145cd3ba8d6b78
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-28 01:12:01 +0000

    Deploy turtlecoin.lol to github.com/turtlecoin/turtlecoin.lol.git:gh-pages

---
+++ it/assets/js/github-data.js
@@ -0,0 +1,60 @@
+$(document)
+  .ready(() => {
+    $.get({
+      url: 'https://api.github.com/repos/turtlecoin/turtlecoin/issues',
+      json: true,
+      success: function (data) {
+        $('#github-issues').html('')
+
+        for (var i = 0; i < 3; i++) {
+          const issue = data.shift()
+
+          $('#github-issues')
+            .append(`<li><a href="${issue.html_url}">${issue.title}</a>`)
+        }
+      }
+    })
+
+    $.get({
+      url: 'https://api.github.com/repos/turtlecoin/turtlecoin/commits',
+      json: true,
+      success: function (data) {
+        $('#github-commits').html('')
+
+        data = data.filter(item => item.commit.message.indexOf('Merge') === -1)
+
+        for (var i = 0; i < 3; i++) {
+          const commit = data.shift()
+
+          $('#github-commits')
+            .append(`<li><a href="${commit.html_url}">${commit.commit.message}</a>`)
+        }
+      }
+    })
+
+    $.get({
+      url: 'https://api.github.com/repos/turtlecoin/turtle-wallet-proton/releases/latest',
+      json: true,
+      success: function (data) {
+        for (var i = 0; i < data.assets.length; i++) {
+          const asset = data.assets[i]
+          switch (asset.content_type) {
+            case 'application/x-ms-dos-executable':
+              $('#gui-windows-dl')
+              .attr("href", asset.browser_download_url)
+              break
+            case 'application/x-apple-diskimage':
+              $('#gui-mac-dl')
+              .attr("href", asset.browser_download_url)
+              break
+            case 'application/vnd.debian.binary-package':
+              $('#gui-linux-dl')
+                .attr("href", asset.browser_download_url)
+              break
+            default:
+              break
+          }
+        }
+      }
+    })
+  })
